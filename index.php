3ee5fa58f4f8f81a756e4da04bac3d400f1d90c2
hiken13 <mancraft13@hotmail.es>
2015-12-21 11:56:35 -0600

    Visor

--- index.php
+++ index.php
@@ -4,16 +4,30 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<scriptsrc></script>
+
 <html>
-    <head>
+    <head ng-app="visorGIS">
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular.min.js"></script>
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
         <meta charset="UTF-8">
         <title>GIS Vission</title>
     </head>
-    <body>
-        <div id="Panel" style="background-color: gray; height:480px; width:640px;position: absolute; left: 350px; top: 100px"></div>
-        <?php
-        // put your code here
-        ?>
-    </body>
+    <center>
+        <body class="bg-info">
+            <div class="btn-group" style=" top: 5%">            
+                <a href="#" class="btn btn-default btn-sm">Cambiar tamaño</a>            
+            </div>  
+            <div ng-view="" class="container" id="Panel" style="background-color: gray; height:480px; width:640px; position: relative; top: 100%">
+
+            </div>
+            <label>Zoom</label>
+            <div class="btn-group" style=" top: 5%">            
+                <a href="#" class="btn btn-default btn-sm">+</a>
+                <a href="#" class="btn btn-default btn-sm">-</a>               
+            </div>  
+    </center>
+    <?php
+    // put your code here
+    ?>
+</body>
 </html>
