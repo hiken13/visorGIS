f483fb0d0ea0b0e0c4607c715da4ee373ed0eea3
hiken13 <mancraft13@hotmail.es>
2015-12-23 01:19:32 -0600

    capa Rios

---
+++ Queries/Rios/graficosRios.php
@@ -0,0 +1,62 @@
+<?php
+
+//namespace graficos;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of newPHPClass
+ *
+ * @author leoviquez
+ */
+class graficos 
+{
+    /**
+     * 
+     * @param int $x : Ancho
+     * @param inty $y : Alto
+     * @return image : i magen resultante
+     */
+    
+    function crearImagen ($x,$y)
+    {
+        $img = imagecreatetruecolor($x,$y);
+
+        $trans = imagecolorallocatealpha($img, 255, 255, 255, 127);
+        $red = imagecolorallocatealpha($img, 255, 0, 0, 63);
+        $blue = imagecolorallocatealpha($img, 0, 0, 255, 63);
+        imagefilltoborder($img, 0, 0, $trans, $trans);
+        imagesavealpha($img, true);
+
+//        imagefilledrectangle($img, 0, 0, 200, 200, $red);
+//        imagefilledrectangle($img, 100, 100, 300, 300, $blue);
+
+        $host='localhost';
+        $db='cursoGIS';
+        $usr='postgres';
+        $pass='12345';
+        
+        $strconn = "host=$host port=5432 dbname=$db user=$usr password=$pass";
+        $conn = pg_connect($strconn) or die("Error de Conexion con la base de datos");
+        
+        $query="select (ST_X(ST_geometryn(r.geom,1)) - 292369.968163136)/564.017324260508 x,
+	640 - (ST_Y(ST_geometryn(r.geom,1))-889242.988534586)/564.017324260508 y
+from (select ((ST_DumpPoints(ST_geometryn(geom,1))).geom) geom from rios) r";
+        
+        
+        
+        $result = pg_query($conn, $query) or die("Error al ejecutar la consulta");
+
+        while ($row=pg_fetch_row($result))
+        {
+            //imagefilledellipse($img, $row[0], $row[1], 10, 10, $red);
+           imagefilledellipse($img, $row[0], $row[1], 1, 1, $blue);
+        }
+
+        return ($img);
+    }
+            
+}
